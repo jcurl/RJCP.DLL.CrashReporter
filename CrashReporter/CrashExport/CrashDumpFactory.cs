f67d8dba2778650d7c47618fcc0cb208cb4c989a
Jason Curl <jcurl@arcor.de>
2019-12-30 15:06:50 +0100

    ICrashDumpFactory: Provide interface for creating dump files
    Implementations making dump files are generally internal, only exposed through the factory. Thus an interface is needed before we can implement the first dumper.
    
    Issue: HELIOS-1275

---
+++ CrashReporter/CrashExport/CrashDumpFactory.cs
@@ -0,0 +1,74 @@
+﻿namespace RJCP.Diagnostics.CrashExport
+{
+    using System;
+    using System.IO;
+#if NET45
+    using System.Threading.Tasks;
+#endif
+
+    /// <summary>
+    /// A factory that knows how to create a dump file.
+    /// </summary>
+    public class CrashDumpFactory : ICrashDumpFactory
+    {
+        /// <summary>
+        /// Creates the dump from the given file name.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile" /> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport" />.
+        /// </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public ICrashDataDumpFile Create(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes the dump file for the given stream.
+        /// </summary>
+        /// <param name="stream">The stream to write dump information to.</param>
+        /// <param name="path">The path where additional dump files can be copied to.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile" /> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport" />.
+        /// </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public ICrashDataDumpFile Create(Stream stream, string path)
+        {
+            throw new NotImplementedException();
+        }
+
+#if NET45
+        /// <summary>
+        /// Creates the dump from the given file name asynchronously.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile" /> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport" />.
+        /// </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<ICrashDataDumpFile> CreateAsync(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes the dump file for the given stream asynchronously.
+        /// </summary>
+        /// <param name="stream">The stream to write dump information to.</param>
+        /// <param name="path">The path where additional dump files can be copied to.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile" /> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport" />.
+        /// </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<ICrashDataDumpFile> CreateAsync(Stream stream, string path)
+        {
+            throw new NotImplementedException();
+        }
+#endif
+    }
+}
---
+++ CrashReporter/CrashExport/ICrashDumpFactory.cs
@@ -0,0 +1,57 @@
+﻿namespace RJCP.Diagnostics.CrashExport
+{
+    using System.IO;
+#if NET45
+    using System.Threading.Tasks;
+#endif
+
+    /// <summary>
+    /// A factory interface for instantiating objects which can write dump files.
+    /// </summary>
+    public interface ICrashDumpFactory
+    {
+        /// <summary>
+        /// Creates the dump from the given file name.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile"/> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport"/>.
+        /// </returns>
+        ICrashDataDumpFile Create(string fileName);
+
+        /// <summary>
+        /// Initializes the dump file for the given stream.
+        /// </summary>
+        /// <param name="stream">The stream to write dump information to.</param>
+        /// <param name="path">The path where additional dump files can be copied to.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile"/> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport"/>.
+        /// </returns>
+        ICrashDataDumpFile Create(Stream stream, string path);
+
+#if NET45
+        /// <summary>
+        /// Creates the dump from the given file name asynchronously.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile"/> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport"/>.
+        /// </returns>
+        Task<ICrashDataDumpFile> CreateAsync(string fileName);
+
+        /// <summary>
+        /// Initializes the dump file for the given stream asynchronously.
+        /// </summary>
+        /// <param name="stream">The stream to write dump information to.</param>
+        /// <param name="path">The path where additional dump files can be copied to.</param>
+        /// <returns>
+        /// An <see cref="ICrashDataDumpFile"/> which can be given to dumpers implementing
+        /// <see cref="ICrashDataExport"/>.
+        /// </returns>
+        Task<ICrashDataDumpFile> CreateAsync(Stream stream, string path);
+#endif
+    }
+}
