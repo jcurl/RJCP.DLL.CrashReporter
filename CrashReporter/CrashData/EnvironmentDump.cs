2777df4f2ab56a4bfd88ac0307e78ebd1c015e25
Jason Curl <jcurl@arcor.de>
2019-12-30 10:48:27 +0100

    EnvironmentDump: Dump all environment variables for the current process
    Issue: HELIOS-1297

---
+++ CrashReporter/CrashData/EnvironmentDump.cs
@@ -0,0 +1,66 @@
+﻿namespace RJCP.Diagnostics.CrashData
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using CrashExport;
+#if NET45
+    using System.Threading.Tasks;
+#endif
+
+    /// <summary>
+    /// Dump all environment variables for the current process.
+    /// </summary>
+    public class EnvironmentDump : ICrashDataExport
+    {
+        private const string EnvTable = "EnvironmentVariables";
+        private const string EnvName = "name";
+        private const string EnvValue = "value";
+
+        /// <summary>
+        /// Dumps debug information using the provided dump interface.
+        /// </summary>
+        /// <param name="dumpFile">The dump interface to write properties to.</param>
+        public void Dump(ICrashDataDumpFile dumpFile)
+        {
+            Dictionary<string, string> variableRow = new Dictionary<string, string>() {
+                { EnvName, string.Empty },
+                { EnvValue, string.Empty }
+            };
+            using (IDumpTable table = dumpFile.DumpTable(EnvTable, "item")) {
+                table.DumpHeader(EnvName, EnvValue);
+
+                foreach (DictionaryEntry variable in Environment.GetEnvironmentVariables()) {
+                    variableRow[EnvName] = variable.Key.ToString();
+                    variableRow[EnvValue] = variable.Value.ToString();
+                    table.DumpRow(variableRow);
+                }
+                table.Flush();
+            }
+        }
+
+#if NET45
+        /// <summary>
+        /// Asynchronously dumps debug information using the provided dump interface.
+        /// </summary>
+        /// <param name="dumpFile">The dump interface to write properties to.</param>
+        public async Task DumpAsync(ICrashDataDumpFile dumpFile)
+        {
+            Dictionary<string, string> variableRow = new Dictionary<string, string>() {
+                { EnvName, string.Empty },
+                { EnvValue, string.Empty }
+            };
+            using (IDumpTable table = await dumpFile.DumpTableAsync(EnvTable, "item")) {
+                await table.DumpHeaderAsync(EnvName, EnvValue);
+
+                foreach (DictionaryEntry variable in Environment.GetEnvironmentVariables()) {
+                    variableRow[EnvName] = variable.Key.ToString();
+                    variableRow[EnvValue] = variable.Value.ToString();
+                    await table.DumpRowAsync(variableRow);
+                }
+                await table.FlushAsync();
+            }
+        }
+#endif
+    }
+}
---
+++ CrashReporterTest/CrashData/EnvironmentDumpTest.cs
@@ -0,0 +1,45 @@
+﻿namespace RJCP.Diagnostics.CrashData
+{
+    using CrashExport;
+    using NUnit.Framework;
+#if NET45
+    using System.Threading.Tasks;
+#endif
+
+    [TestFixture(Category = "CrashReporter.CrashData")]
+    public class EnvironmentDumpTest
+    {
+        [Test]
+        public void EnvVarDump()
+        {
+            using (MemoryCrashDataDumpFile dumpFile = new MemoryCrashDataDumpFile()) {
+                EnvironmentDump dump = new EnvironmentDump();
+                dump.Dump(dumpFile);
+                dumpFile.Flush();
+
+                Assert.That(CheckDump(dumpFile), Is.True);
+            }
+        }
+
+        private bool CheckDump(MemoryCrashDataDumpFile dumpFile)
+        {
+            dumpFile.DumpContent();
+            Assert.That(dumpFile["EnvironmentVariables"].Count, Is.Not.EqualTo(0));
+            return true;
+        }
+
+#if NET45
+        [Test]
+        public async Task EnvVarDumpAsync()
+        {
+            using (MemoryCrashDataDumpFile dumpFile = new MemoryCrashDataDumpFile()) {
+                EnvironmentDump dump = new EnvironmentDump();
+                await dump.DumpAsync(dumpFile);
+                await dumpFile.FlushAsync();
+
+                Assert.That(CheckDump(dumpFile), Is.True);
+            }
+        }
+#endif
+    }
+}
