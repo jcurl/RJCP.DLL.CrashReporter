c9796bbb85153ee51cb2f60028daa585041bf25f
Jason Curl <jcurl@arcor.de>
2019-12-30 11:47:09 +0100

    AssemblyDump: Dump all assemblies in the current domain
    Issue: HELIOS-1281

---
+++ CrashReporter/CrashData/AssemblyDump.cs
@@ -0,0 +1,107 @@
+﻿namespace RJCP.Diagnostics.CrashData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using CrashExport;
+#if NET45
+    using System.Threading.Tasks;
+#endif
+
+    /// <summary>
+    /// Dumps details of all loaded assemblies in the current domain.
+    /// </summary>
+    /// <seealso cref="RJCP.Diagnostics.CrashExport.ICrashDataExport" />
+    public class AssemblyDump : ICrashDataExport
+    {
+        private const string AssemblyTable = "Assemblies";
+        private const string AssemblyName = "name";
+        private const string AssemblyFullName = "fullname";
+        private const string AssemblyVersion = "version";
+        private const string AssemblyInfoVersion = "versioninfo";
+        private const string AssemblyFileVersion = "versionfile";
+        private const string AssemblyLocation = "location";
+        private const string AssemblyCodeBase = "codebase";
+        private const string AssemblyProcessor = "processor";
+
+        private Dictionary<string, string> m_Row = new Dictionary<string, string>() {
+                { AssemblyName, string.Empty },
+                { AssemblyVersion, string.Empty },
+                { AssemblyFullName, string.Empty },
+                { AssemblyInfoVersion, string.Empty },
+                { AssemblyFileVersion, string.Empty },
+                { AssemblyProcessor, string.Empty },
+                { AssemblyLocation, string.Empty },
+                { AssemblyCodeBase, string.Empty }
+            };
+
+        /// <summary>
+        /// Dumps debug information using the provided dump interface.
+        /// </summary>
+        /// <param name="dumpFile">The dump interface to write properties to.</param>
+        public void Dump(ICrashDataDumpFile dumpFile)
+        {
+            using (IDumpTable table = dumpFile.DumpTable(AssemblyTable, "assembly")) {
+                table.DumpHeader(
+                    AssemblyName, AssemblyVersion, AssemblyFullName, AssemblyInfoVersion,
+                    AssemblyFileVersion, AssemblyProcessor, AssemblyLocation, AssemblyCodeBase);
+
+
+                AppDomain domain = AppDomain.CurrentDomain;
+                foreach (Assembly assembly in domain.GetAssemblies()) {
+                    GetAssemblyInformation(assembly, m_Row);
+                    table.DumpRow(m_Row);
+                }
+                table.Flush();
+            }
+        }
+
+        private void GetAssemblyInformation(Assembly assembly, Dictionary<string, string> assemblyRow)
+        {
+            assemblyRow[AssemblyName] = assembly.GetName().Name;
+            assemblyRow[AssemblyFullName] = assembly.FullName;
+            assemblyRow[AssemblyVersion] = assembly.GetName().Version.ToString();
+            assemblyRow[AssemblyInfoVersion] = GetAssemblyInformationalVersion(assembly);
+            assemblyRow[AssemblyFileVersion] = GetAssemblyFileVersion(assembly);
+            assemblyRow[AssemblyLocation] = assembly.Location;
+            assemblyRow[AssemblyCodeBase] = assembly.CodeBase;
+            assemblyRow[AssemblyProcessor] = assembly.GetName().ProcessorArchitecture.ToString();
+        }
+
+        private string GetAssemblyInformationalVersion(Assembly assembly)
+        {
+            if (!(Attribute.GetCustomAttribute(assembly, typeof(AssemblyInformationalVersionAttribute))
+                is AssemblyInformationalVersionAttribute infoVersion)) return null;
+            return infoVersion.InformationalVersion;
+        }
+
+        private string GetAssemblyFileVersion(Assembly assembly)
+        {
+            if (!(Attribute.GetCustomAttribute(assembly, typeof(AssemblyFileVersionAttribute))
+                is AssemblyFileVersionAttribute fileVersion)) return null;
+            return fileVersion.Version;
+        }
+
+#if NET45
+        /// <summary>
+        /// Asynchronously dumps debug information using the provided dump interface.
+        /// </summary>
+        /// <param name="dumpFile">The dump interface to write properties to.</param>
+        public async Task DumpAsync(ICrashDataDumpFile dumpFile)
+        {
+            using (IDumpTable table = await dumpFile.DumpTableAsync(AssemblyTable, "assembly")) {
+                await table.DumpHeaderAsync(
+                    AssemblyName, AssemblyVersion, AssemblyFullName, AssemblyInfoVersion,
+                    AssemblyFileVersion, AssemblyProcessor, AssemblyLocation, AssemblyCodeBase);
+
+                AppDomain domain = AppDomain.CurrentDomain;
+                foreach (Assembly assembly in domain.GetAssemblies()) {
+                    GetAssemblyInformation(assembly, m_Row);
+                    await table.DumpRowAsync(m_Row);
+                }
+                await table.FlushAsync();
+            }
+        }
+#endif
+    }
+}
---
+++ CrashReporterTest/CrashData/AssemblyDumpTest.cs
@@ -0,0 +1,54 @@
+﻿namespace RJCP.Diagnostics.CrashData
+{
+    using System;
+    using System.Reflection;
+    using CrashExport;
+    using NUnit.Framework;
+#if NET45
+    using System.Threading.Tasks;
+#endif
+
+    [TestFixture(Category = "CrashReporter.CrashData")]
+    public class AssemblyDumpTest
+    {
+        [Test]
+        public void AssemblyDump()
+        {
+            using (MemoryCrashDataDumpFile dumpFile = new MemoryCrashDataDumpFile()) {
+                AssemblyDump dump = new AssemblyDump();
+                dump.Dump(dumpFile);
+                dumpFile.Flush();
+
+                Assert.That(CheckDump(dumpFile), Is.True);
+            }
+        }
+
+        private bool CheckDump(MemoryCrashDataDumpFile dumpFile)
+        {
+            dumpFile.DumpContent();
+            Assert.That(dumpFile["Assemblies"].Count, Is.Not.EqualTo(0));
+
+            bool found = false;
+            Assembly assemblyDumpAssembly = typeof(AssemblyDump).Assembly;
+            foreach (var row in dumpFile["Assemblies"]) {
+                if (row["name"].Equals(assemblyDumpAssembly.GetName().Name)) found = true;
+            }
+            Assert.That(found, Is.True, "Couldn't find assembly '{0}' in dump", assemblyDumpAssembly.GetName().Name);
+            return true;
+        }
+
+#if NET45
+        [Test]
+        public async Task AssemblyDumpAsync()
+        {
+            using (MemoryCrashDataDumpFile dumpFile = new MemoryCrashDataDumpFile()) {
+                AssemblyDump dump = new AssemblyDump();
+                await dump.DumpAsync(dumpFile);
+                await dumpFile.FlushAsync();
+
+                Assert.That(CheckDump(dumpFile), Is.True);
+            }
+        }
+#endif
+    }
+}
