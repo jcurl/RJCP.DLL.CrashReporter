f7d86d9f9046fbe22394a5e5e364a40e13d25141
Jason Curl <jcurl@arcor.de>
2020-01-10 19:52:07 +0100

    CrashDump: Compress using System.IO.Compression with .NET 4.5
    Issue: HELIOS-1293

---
+++ CrashReporter/Dump/Archive/CompressZipArchive.cs
@@ -0,0 +1,50 @@
+﻿namespace RJCP.Diagnostics.Dump.Archive
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    internal static class Compress
+    {
+        public static bool CompressFolder(string path)
+        {
+            if (path == null) return false;
+            if (!Directory.Exists(path)) return false;
+
+            DirectoryInfo sourceDir = new DirectoryInfo(path);
+            string zipFileName = string.Format("{0}.zip", path);
+
+            using (ZipArchive archive = ZipFile.Open(zipFileName, ZipArchiveMode.Create)) {
+                ZipDirectoryTree(archive, sourceDir, Path.GetFileName(path));
+            }
+
+            return true;
+        }
+
+        private static void ZipDirectoryTree(ZipArchive archive, DirectoryInfo sourceDir, string zipDir)
+        {
+            FileInfo[] files = null;
+            try {
+                files = sourceDir.GetFiles();
+            } catch (UnauthorizedAccessException) {
+                // Ignore, as it shouldn't happen
+            } catch (DirectoryNotFoundException) {
+                // Ignore, as it shouldn't happen
+            } catch (PathTooLongException) {
+                // Ignore, as it shouldn't happen
+            }
+            if (files != null) {
+                foreach (FileInfo file in files) {
+                    string zipEntry = Path.Combine(zipDir, file.Name);
+                    archive.CreateEntryFromFile(file.FullName, zipEntry, CompressionLevel.Optimal);
+                }
+            }
+
+            DirectoryInfo[] subDirs = sourceDir.GetDirectories();
+            foreach (DirectoryInfo subDir in subDirs) {
+                string subDirPath = Path.Combine(zipDir, subDir.Name);
+                ZipDirectoryTree(archive, subDir, subDirPath);
+            }
+        }
+    }
+}
