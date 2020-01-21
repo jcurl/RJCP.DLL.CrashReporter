e848fd703e7d0c5fc951a969976265d04a452707
Jason Curl <jcurl@arcor.de>
2020-01-21 21:06:40 +0100

    Use default configuration in case of an error
    Issue: HELIOS-1335

--- CrashReporter/AppConfig.cs
+++ CrashReporter/AppConfig.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Configuration;
+    using System.Diagnostics;
 
     internal class AppConfig
     {
@@ -25,32 +26,38 @@
 
         private AppConfig()
         {
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            ConfigurationSectionGroup grp;
+            Configuration config;
+            ConfigurationSectionGroup grp = null;
             try {
-                grp = config.SectionGroups["CrashReporter"];
-            } catch (ArgumentException) {
-                // The CrashReporter group is not defined, so ignore it. Although this is probably because the
-                // section <CrashReporter> is present, but not the <sectionGroup name="CrashReporter">. Observed
-                // that crashes occur somewhere else later anyway when single-stepping.
-                return;
-            }
+                config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                try {
+                    grp = config.SectionGroups["CrashReporter"];
+                } catch (ArgumentException) {
+                    // The CrashReporter group is not defined, so ignore it. Although this is probably because the
+                    // section <CrashReporter> is present, but not the <sectionGroup name="CrashReporter">. Observed
+                    // that crashes occur somewhere else later anyway when single-stepping.
+                    return;
+                }
 
-            if (grp != null) {
-                foreach (ConfigurationSection section in grp.Sections) {
-                    try {
-                        switch (section.SectionInformation.Name) {
-                        case "XmlCrashDumper":
-                            XmlCrashDumper = new Config.CrashReporter.XmlCrashDumperConfig((Config.XmlCrashDumper)grp.Sections["XmlCrashDumper"]);
-                            break;
-                        case "Watchdog":
-                            Watchdog = new Config.CrashReporter.WatchdogConfig((Config.Watchdog)grp.Sections["Watchdog"]);
-                            break;
+                if (grp != null) {
+                    foreach (ConfigurationSection section in grp.Sections) {
+                        try {
+                            switch (section.SectionInformation.Name) {
+                            case "XmlCrashDumper":
+                                XmlCrashDumper = new Config.CrashReporter.XmlCrashDumperConfig((Config.XmlCrashDumper)grp.Sections["XmlCrashDumper"]);
+                                break;
+                            case "Watchdog":
+                                Watchdog = new Config.CrashReporter.WatchdogConfig((Config.Watchdog)grp.Sections["Watchdog"]);
+                                break;
+                            }
+                        } catch (ConfigurationException) {
+                            // Ignore exception errors. They'll be logged by default in the FirstChance.
                         }
-                    } catch (ConfigurationException) {
-                        // Ignore exception errors. They'll be logged by default in the FirstChance.
                     }
                 }
+            } catch (ConfigurationErrorsException ex) {
+                // There was an error loading the configuration, so we use the default configuration
+                Log.CrashLog.TraceEvent(TraceEventType.Warning, 0, "Error loading configuration: {0}", ex.Message);
             }
 
             // Provide default implementations is it is not in the configuration. Code doesn't need to check for 'null'
