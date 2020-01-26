a788f99e265b5c445d7317891e8b09f2f0734c42
Jason Curl <jcurl@arcor.de>
2020-01-26 15:26:37 +0100

    Update version to 0.4.0
    Issue: HELIOS-1314

--- CrashReporter/Properties/AssemblyInfo.cs
+++ CrashReporter/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.0.0")]
 [assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyInformationalVersion("0.4.0-alpha.1")]
+[assembly: AssemblyInformationalVersion("0.4.0.0")]
