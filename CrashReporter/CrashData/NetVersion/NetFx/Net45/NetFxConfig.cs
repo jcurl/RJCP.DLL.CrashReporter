edd6ff9a19bcfbbd9882e39a8a19bdfb30f0bac6
Jason Curl <jcurl@arcor.de>
2020-02-08 11:48:17 +0100

    DotNET Dump: Update detection of .NET 4.8 on Windows 1909
    Use the new information in pull request https://github.com/dotnet/docs/pull/16967, to detect .NET on Windows 1909.
    
    Issue: HELIOS-1370

--- CrashReporter/CrashData/NetVersion/NetFx/Net45/NetFxConfig.cs
+++ CrashReporter/CrashData/NetVersion/NetFx/Net45/NetFxConfig.cs
@@ -11,18 +11,19 @@
             { 379893, new NetFxVersion("4.5.2", ".NET Framework 4.5.2") },
             { 393295, new NetFxVersion("4.6", ".NET Framework 4.6 on Windows 10 (1507)") },
             { 393297, new NetFxVersion("4.6", ".NET Framework 4.6") },
-            { 394254, new NetFxVersion("4.6.1", ".NET Framework 4.6.1 nn Windows 10 November Update systems (1511)") },
+            { 394254, new NetFxVersion("4.6.1", ".NET Framework 4.6.1 nn Windows 10, November Update systems (1511)") },
             { 394271, new NetFxVersion("4.6.1", ".NET Framework 4.6.1") },
-            { 394802, new NetFxVersion("4.6.2", ".NET Framework 4.6.2 on Windows 10 Anniversary Update (1607)") },
+            { 394802, new NetFxVersion("4.6.2", ".NET Framework 4.6.2 on Windows 10, Anniversary Update (1607) and Windows Server 2016") },
             { 394806, new NetFxVersion("4.6.2", ".NET Framework 4.6.2") },
-            { 460798, new NetFxVersion("4.7", ".NET Framework 4.7 on Windows 10 Creators Update (1703)") },
+            { 460798, new NetFxVersion("4.7", ".NET Framework 4.7 on Windows 10, Creators Update (1703)") },
             { 460805, new NetFxVersion("4.7", ".NET Framework 4.7") },
-            { 461308, new NetFxVersion("4.7.1", ".NET Framework 4.7.1 on Windows 10 Fall Creators Update (1709)") },
+            { 461308, new NetFxVersion("4.7.1", ".NET Framework 4.7.1 on Windows 10, Fall Creators Update (1709)") },
             { 461310, new NetFxVersion("4.7.1", ".NET Framework 4.7.1") },
-            { 461808, new NetFxVersion("4.7.2", ".NET Framework 4.7.2 on Windows 10 Spring Creators Update (1803)") },
+            { 461808, new NetFxVersion("4.7.2", ".NET Framework 4.7.2 on Windows 10, Spring Creators Update (1803)") },
             { 461814, new NetFxVersion("4.7.2", ".NET Framework 4.7.2") },
-            { 528040, new NetFxVersion("4.8", ".NET Framework 4.8 on Windows 10 May 2019 Update (1903)") },
-            { 528049, new NetFxVersion("4.8", ".NET Framework 4.8") }
+            { 528040, new NetFxVersion("4.8", ".NET Framework 4.8 on Windows 10, May 2019 Update (1903)") },
+            { 528049, new NetFxVersion("4.8", ".NET Framework 4.8") },
+            { 528209, new NetFxVersion("4.8", ".NET Framework 4.8 on Windows 10, October 2019 Update (1909)") }
         };
 
         public static NetFxVersion GetNetFxVersion(int release)
