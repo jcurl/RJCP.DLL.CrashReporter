8ed4ea1e86cdca360c1eb68d72157318d0a006cf
Jason Curl <jcurl@arcor.de>
2021-08-17 09:04:05 +0200

    .NET Version: Get the runtime from the framework if available
    On .NET Core and .NET Standard, there's a new API called RuntimeInformation.FrameworkDescription. Use this to provide the description of the running framework, instead of a hardcoded string. On .NET Framework projects, this remains as the hard coded string as the API doesn't exist.
    
    Issue: DOTNET-366

--- CrashReporter/CrashData/NetVersion/Runtime/NetFx.cs
+++ CrashReporter/CrashData/NetVersion/Runtime/NetFx.cs
@@ -1,13 +1,20 @@
 ﻿namespace RJCP.Diagnostics.CrashData.NetVersion.Runtime
 {
     using System;
+#if NETSTANDARD
+    using System.Runtime.InteropServices;
+#endif
 
     internal class NetFx : INetVersion
     {
         public NetFx()
         {
             Version = Environment.Version.ToString();
+#if !NETSTANDARD
             Description = ".NET Environment Runtime";
+#else
+            Description = RuntimeInformation.FrameworkDescription;
+#endif
             IsValid = true;
         }
 
