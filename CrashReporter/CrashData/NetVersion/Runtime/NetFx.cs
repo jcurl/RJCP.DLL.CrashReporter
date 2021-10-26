a990840ef22f2f97f75ee5790ad1b7afcf78ea9d
Jason Curl <jcurl@arcor.de>
2021-10-26 11:49:26 +0200

    CrashReporter: .NET Runtime support for .NET 4.7.1 and later

--- CrashReporter/CrashData/NetVersion/Runtime/NetFx.cs
+++ CrashReporter/CrashData/NetVersion/Runtime/NetFx.cs
@@ -3,6 +3,8 @@
     using System;
 #if NETSTANDARD
     using System.Runtime.InteropServices;
+#else
+    using System.Reflection;
 #endif
 
     internal class NetFx : INetVersion
@@ -10,10 +12,19 @@
         public NetFx()
         {
             Version = Environment.Version.ToString();
-#if !NETSTANDARD
-            Description = ".NET Environment Runtime";
-#else
+#if NETSTANDARD
             Description = RuntimeInformation.FrameworkDescription;
+#else
+            // This information is available in .NET 4.7.1 and later. But as we also target earlier frameworks, get this
+            // information dynamically.
+            Type rti = Type.GetType("System.Runtime.InteropServices.RuntimeInformation");
+            if (rti != null) {
+                PropertyInfo property = rti.GetProperty("FrameworkDescription");
+                Description = property.GetValue(null, null) as string;
+            }
+
+            if (Description == null)
+                Description = ".NET Environment Runtime";
 #endif
             IsValid = true;
         }
