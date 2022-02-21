jcurl/RJCP.DLL.CrashReporter

commit 7da79714d39ac08ee7ec342ed8128d83f440c3b6

XmlCrashDumper: Sanitize output before writing for Async methods
The fix provided in af2813d ("XmlCrashDumper: Sanitize output before writing") only did this when writing output using .NET 4.0 and non-async methods. The same problem would occur with asynchronous methods, found when running the nunit3-console.exe runner under a tool like Cmdr.

Crash dumps would fail with an incomplete XML output when a specific environment variable which contained control characters, such as the 'backspace' key. The XmlWriter would enter an error state and crash dumps could not be performed.

Issue: HELIOS-1663

CrashReporter/Dump/Crash.cs
@@ -253,14 +253,18 @@
         private async Task DumpAsync(ICrashDataDumpFile dump)
         {
             if (dump.IsSynchronous) {
+                Console.WriteLine("DumpAsync: IsSynchronous");
                 foreach (ICrashDataExport dumper in Providers) {
+                    Console.WriteLine("DumpAsync: Dump {0}", dumper.ToString());
                     await dumper.DumpAsync(dump);
                 }
             } else {
+                Console.WriteLine("DumpAsync: !IsSynchronous");
                 List<Task> dumpers = new List<Task>(Providers.Count);
                 foreach (ICrashDataExport dumper in Providers) {
                     dumpers.Add(dumper.DumpAsync(dump));
                 }
+                Console.WriteLine("DumpAsync: !IsSynchronous waiting for all");
                 await Task.WhenAll(dumpers);
             }
             await dump.FlushAsync();

CrashReporter/CrashExport/Xml/XmlDumpTable.cs
@@ -65,7 +65,7 @@
             try {
                 IEnumerable<string> fields = m_Fields ?? row.Keys;
                 foreach (string field in fields) {
-                    await m_Writer.WriteAttributeStringAsync(null, field, null, row[field]);
+                    await m_Writer.WriteAttributeStringAsync(null, field, null, XmlExtensions.SanitizeXml10(row[field]));
                 }
             } finally {
                 await m_Writer.WriteEndElementAsync();

CrashReporter/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.2.0")]
-[assembly: AssemblyFileVersion("0.4.2.0")]
-[assembly: AssemblyInformationalVersion("0.4.2.0")]
+[assembly: AssemblyVersion("0.4.3.0")]
+[assembly: AssemblyFileVersion("0.4.3.0")]
+[assembly: AssemblyInformationalVersion("0.4.3")]

CrashReporterTest/packages.RJCP.CrashReporterTest-net40.config
@@ -2,7 +2,7 @@
 <packages>
   <package id="NUnit" version="3.12.0" targetFramework="net40" />
   <package id="NUnit.Analyzers" version="0.1.0-dev-00216" targetFramework="net40" />
-  <package id="NUnit.ConsoleRunner" version="3.10.0" targetFramework="net40" />
+  <package id="NUnit.ConsoleRunner" version="3.11.1" targetFramework="net40" />
   <package id="NUnit3TestAdapter" version="3.16.1" targetFramework="net40" developmentDependency="true" />
   <package id="NUnitTestAdapter" version="2.3.0" targetFramework="net40" />
 </packages>

CrashReporterTest/packages.RJCP.CrashReporterTest-net45.config
@@ -2,7 +2,7 @@
 <packages>
   <package id="NUnit" version="3.12.0" targetFramework="net45" />
   <package id="NUnit.Analyzers" version="0.1.0-dev-00216" targetFramework="net45" />
-  <package id="NUnit.ConsoleRunner" version="3.10.0" targetFramework="net45" />
+  <package id="NUnit.ConsoleRunner" version="3.11.1" targetFramework="net45" />
   <package id="NUnit3TestAdapter" version="3.16.1" targetFramework="net45" developmentDependency="true" />
   <package id="NUnitTestAdapter" version="2.3.0" targetFramework="net45" />
 </packages>

commit 1e08495327b51e8c57453d183624f5bfbc216254

Crash: Remove unnecessary debug output
Remove debug output, introduced with 7da7971 ("XmlCrashDumper: Sanitize output before writing for Async methods").

Issue: DOTNET-571

CrashReporter/Dump/Crash.cs
@@ -253,18 +253,14 @@
         private async Task DumpAsync(ICrashDataDumpFile dump)
         {
             if (dump.IsSynchronous) {
-                Console.WriteLine("DumpAsync: IsSynchronous");
                 foreach (ICrashDataExport dumper in Providers) {
-                    Console.WriteLine("DumpAsync: Dump {0}", dumper.ToString());
                     await dumper.DumpAsync(dump);
                 }
             } else {
-                Console.WriteLine("DumpAsync: !IsSynchronous");
                 List<Task> dumpers = new List<Task>(Providers.Count);
                 foreach (ICrashDataExport dumper in Providers) {
                     dumpers.Add(dumper.DumpAsync(dump));
                 }
-                Console.WriteLine("DumpAsync: !IsSynchronous waiting for all");
                 await Task.WhenAll(dumpers);
             }
             await dump.FlushAsync();