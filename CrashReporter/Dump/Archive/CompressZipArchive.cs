758541a26719ee96e2aefca7d6356d6014e08e46
Jason Curl <jcurl@arcor.de>
2022-07-24 18:34:44 +0200

    CrashReporter: Create file permissions in ZIP with .NET 4.6.1 and later
    On Ubuntu 20.04 with .NET Core 3.1, files are created that have no permissions and not readable when extracted. Set the `ExternalAttributes` field for Unix bits on .NET 4.6.1 and later (and .NET Standard 2.1 and later). Permissions are set to 640 (rw-r-----) and verified with 7-Zip, and extracting.
    
    Issue: DOTNET-646

--- CrashReporter/Dump/Archive/CompressZipArchive.cs
+++ CrashReporter/Dump/Archive/CompressZipArchive.cs
@@ -3,6 +3,7 @@
     using System;
     using System.IO;
     using System.IO.Compression;
+    using System.Reflection;
 
     internal static class Compress
     {
@@ -34,7 +35,7 @@
             if (files != null) {
                 foreach (FileInfo file in files) {
                     string zipEntry = Path.Combine(zipDir, file.Name);
-                    archive.CreateEntryFromFile(file.FullName, zipEntry, CompressionLevel.Optimal);
+                    _ = CreateEntryFromFile(archive, file.FullName, zipEntry);
                 }
             }
 
@@ -43,6 +44,46 @@
                 string subDirPath = Path.Combine(zipDir, subDir.Name);
                 ZipDirectoryTree(archive, subDir, subDirPath);
             }
+        }
+
+        private static ZipArchiveEntry CreateEntryFromFile(ZipArchive archive, string sourceFileName, string entryName)
+        {
+            ZipArchiveEntry entry = archive.CreateEntryFromFile(sourceFileName, entryName, CompressionLevel.Optimal);
+
+            // This only exists on .NET 4.7.2 or later
+            // Permissions 0640 = 1 1010 0000
+            // 0100640 = 1 000 000 110 100 000 = 81A0
+            //
+            // Bitfields, see https://www.gnu.org/software/libc/manual/html_node/Permission-Bits.html
+            //   S_IRUSR = 0400  S_IWUSR = 0200  S_IXUSR = 0100
+            //   S_IRGRP = 0040  S_IWGRP = 0020  S_IXGRP = 0010
+            //   S_IROTH = 0004  S_IWOTH = 0002  S_IXOTH = 0001
+            //   S_ISUID = 4000  S_ISGID = 2000  S_ISVTX = 1000 (sticky)
+            //
+            //   S_IFIFO  0010000  /* named pipe (fifo) */
+            //   S_IFCHR  0020000  /* character special */
+            //   S_IFDIR  0040000  /* directory */
+            //   S_IFBLK  0060000  /* block special */
+            //   S_IFREG  0100000  /* regular */
+            //   S_IFLNK  0120000  /* symbolic link */
+            //   S_IFSOCK 0140000  /* socket */
+            const int unixAttributes = 0x81A0;
+
+#if NETSTANDARD || NET471_OR_GREATER
+            entry.ExternalAttributes = (entry.ExternalAttributes & 0xFFFF) | (unixAttributes << 16);
+#else
+            Type rti = typeof(ZipArchiveEntry);
+
+            // This information is available in .NET 4.7.1 and later. But as we also target earlier frameworks, get this
+            // information dynamically.
+            PropertyInfo property = rti.GetProperty("ExternalAttributes");
+            if (property == null) return entry;
+
+            int attributes = (int)property.GetValue(entry);
+            attributes = (attributes & 0xFFFF) | (unixAttributes << 16);
+            property.SetValue(entry, attributes);
+#endif
+            return entry;
         }
     }
 }
